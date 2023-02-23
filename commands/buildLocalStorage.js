5b10c218e9bfbe6fa8d4ab765d9939bd50a45129
jperdomoNerd <jperdomo.nerd@gmail.com>
2023-02-23 15:21:54 -0500

    Set localStorage for first run project

---
+++ commands/buildLocalStorage.js
@@ -0,0 +1,24 @@
+if (window.localStorage.getItem('defaultValues') === null) {
+    window.localStorage.setItem('defaultValues', JSON.stringify({
+        merchant: 'MzrC/gl6VwS67JrIE+Kyng==',
+        secretKey: '7f127fdad955567d93861c206027e6b7796592645692c77375666ef00629b230',
+        email: 'julian.perdomo@cenposd365.com',
+        cardNumber: '4545454555455',
+        expirationDateMonth: '12',
+        expirationDateYear: '23',
+        cardHolderName: 'Julian Perdomo',
+        tokenId: 'CA7F8LYG',
+        cryptoTokenId: 'CRYPTO%eyJDcnlwdG8iOiJyMFA0WURpTk5QYnVaQnJaN1R0ZTlLWlNrb2ZQUFNYVWt3VHJ4V2YrT3l6RWdGUEFoUURcL2NsbThSM0hGU1pcL1QiLCJTYWx0IjoidlViRmF1S1hwWGkzVEZqMGd4UzFWbjJiSGREUDg4OFJCQzhOXC9iTnFMQzJtbjZuZzVoOTBtTFhreXFKMUE3R3R0WXlSd3NzM256bnVIWXJhMFp5QTZnPT0iLCJTZXNzaW9uIjoiTVRjM2ZEWXpPREV4TWpreE5UVXpNelF3TlRVMk1BPT0iLCJWZXJzaW9uIjoiQUVTIEhNQUMgMC4yIn0=',
+        amount: '50',
+        invoiceNumber: '123456',
+        type: 'Auth',
+        city: 'Chicago',
+        state: 'Illinois',
+        address: '121 N. LaSalle Street',
+        zipCode: '6060255',
+        referenceNumber: '2310420327',
+        verifyingPost: '',
+        isCrypto: 'false',
+        merchants: []
+    }))
+}
